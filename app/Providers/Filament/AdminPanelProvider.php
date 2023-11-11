be943f4583cfe55e1801a77b7aa71d7f7a3dab01
muhammadfaris626 <muhammadfaris5795@gmail.com>
2023-11-12 05:25:06 +0800

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('')
             ->sidebarCollapsibleOnDesktop()
             ->login(PagesLogin::class)
             ->colors([
